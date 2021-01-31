67bc2023bf0aa8ca6e9dfeb2652482e8815b1479
Jade <applegreengrapeintherain@gmail.com>
2021-01-31 20:22:03 +0000

    added a search bar

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,58 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-import React from 'react';
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
 import fetch from 'isomorphic-fetch';
 
-const HOST_NAME = process.env.HOST_NAME || 'http://localhost:8080/events/600000';
+const HOST_NAME = process.env.HOST_NAME || 'http://localhost:8080/events';
 
-export default class extends React.Component {
-    static async getInitialProps() {
-        const data = await fetch(`${HOST_NAME}`);
-        const item = await data.json();
-        return {
-            item
+const Home = () => {
+    const [input, setInput] = useState("");
+    const [results, setResults] = useState([]);
+
+    const handleInput = e => {
+        const txt = e.target.value;
+        setInput(txt);
+      };
+    
+    const handleSearch = async () => {
+        try {
+            const res = await fetch(`${HOST_NAME}/${input}`, {
+                method: "GET",
+                headers: {
+                  "Content-type": "application/json"
+                }
+              });
+            const data = await res.json();
+            setResults(data)
+        }catch (e) {
+            console.log(e)
         };
-    }
-    render() {
-        const { item } = this.props;
-        return (
-            <section>
-                <h1>📇 company profile</h1>
-                <ul>sse code: { item.CODE }</ul>
-                <ul>company name: { item.CompanyName }</ul>
-                <ul>company english name: { item.CompanyNameEn }</ul>
-                <ul>city: { item.Area }</ul>
-                <ul>address: { item.CompanyAddr }</ul>
-            </section>
-        );
-    }
-}
+    };
+
+    return (
+        <div>
+        <h1>📇 company profile</h1>
+          <label>
+          🔎 <input placeholder="600000" value={input} onChange={handleInput} onkeyup={handleSearch}/>
+          <button onClick={_ => handleSearch()}>search</button>
+          </label>
+          <ul>sse code: { results.CODE }</ul>
+          <ul>company name: { results.CompanyName }</ul>
+          <ul>company english name: { results.CompanyNameEn }</ul>
+          <ul>city: { results.Area }</ul>
+          <ul>address: { results.CompanyAddr }</ul>
+        </div>
+
+    );
+};
+
+/*
+Home.getInitialProps = async () => {
+  const res = await fetch(`${HOST_NAME}/600000`);
+  const data = await res.json();
+
+  return { propItems: data };
+};
+*/
+
+export default Home;
